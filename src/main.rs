3eb4ec8e3f5105bfa489193ba5a1bac8ffeebfca
Jake Nulty <nultinator@yahoo.com>
2023-03-14 11:21:34 -0400

    add:
    -runtime loop
    -scheduled arbitrage

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,13 @@ use reqwest::header::{CONTENT_TYPE, HeaderMap, HeaderValue};
 
 use tokio::stream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-//use tokio_tungstenite::{connect_async, tungstenite::protocol::Message};
-use futures::{future, pin_mut, StreamExt};
+use tokio::sync::watch::error;
+use tungstenite;
+use futures::{future, pin_mut, StreamExt, FutureExt};
 
-use websocket::client::ClientBuilder;
-use websocket::{Message, OwnedMessage};
 
+use std::io::Read;
+use std::rc::Rc;
 use std::{time::SystemTime, time::UNIX_EPOCH, collections::HashMap, env};
 
 
@@ -23,19 +24,58 @@ use sha2::Sha256;
 
 mod utils;
 mod binance_us_api;
+mod strategies;
 
 ///////////MAIN PROGRAM////////////
 #[tokio::main]
 async fn main() {
+    let strategies: [&str; 1] = ["scheduled_arb"];
 
-    //Arbitrage function below for testing
-    //WARNING THIS THING USES REAL MONEY AND WILL ACTUALLY SPEND YOUR CRYPTO
-    println!("Binance US API Arbitrage Test");
-
-
-    //attempt arbitrage 
-    binance_us_api::arbitrage("ADA", &["BTC", "ETH", "USDC", "USDT"], 1.05).await;
+    println!("Please select a strategy from the list: ");
+    for strategy in strategies.iter() {
+        let mut counter: i32 = 0;
+    println!("{} {}",counter, strategy);
+        counter += 1;
+    }
+    let resp = utils::get_input().parse::<i32>().unwrap();
+    println!("You have selected: {}",resp);
+    match resp {
+        0 => strategies::scheduled_arb().await,
+        __=> println!("Please select a valid strategy"),
+    }
 
 }
+    //Arbitrage function below for testing
+    //binance_us_api::arbitrage("ADA", &["BTC", "ETH", "USDC", "USDT"], 1.05  ).await;
+    //println!("Binance US API Arbitrage Test");
+
+
+    //Websocket Code....NOT WORKING YET
+    /*
+    let url = Url::parse("wss://binance.us:9443/ws/adabtc@bookTicker").unwrap();
+
+    let request = Client::connect(url).unwrap();
+
+    let response = request.send().unwrap();
+
+    response.validate().unwrap();
+
+    let mut client = response.begin();
+
+    let (mut sender, mut receiver) = client.split();
+
+    for message in receiver.incoming_messages() {
+        let message: Message = message.unwrap();
+        println!("Message: {:?}", message);
+    }
+
+
+
+    //let mut running = true;
+
+    //println!("Connected to socket");
+    */
+
+    
 ////////////////END MAIN////////////////////
 
